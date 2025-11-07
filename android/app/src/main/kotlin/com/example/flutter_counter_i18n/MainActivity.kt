023d22b7cd879bf56718a3595335838a8542c9bc
Soheil Jashnsaz <soheil.8221@gmail.com>
2025-11-07 13:32:11 +0330

    feat: initialize Flutter project structure for multilingual counter app

---
+++ android/app/src/main/kotlin/com/example/flutter_counter_i18n/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_counter_i18n
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_counter_i18n/home_page.dart';
+
+void main() {
+  runApp(const Application());
+}
+
+class Application extends StatelessWidget {
+  const Application({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
